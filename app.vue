efcbb390c1f89b4239674262426dd82ddb1a91ec
downtomarsguy <theonesilverest@gmail.com>
2025-02-20 12:50:41 +0530

    Dishes Added; Navbar Updated: Logo added

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-green-500">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="flex justify-between items-center p-4 bg-[#FFF1BE] shadow-md">
+      <div class="flex items-center space-x-2">
+        <img src="~/assets/dish_full_fit.png" alt="Dish Icon" class="h-8" />
+        <span class="text-xl font-bold">Generic Socionics Site</span>
+      </div>
+    </nav>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const isDarkMode = ref(false);
+  
+  const toggleDarkMode = () => {
+    isDarkMode.value = !isDarkMode.value;
+  };
+  </script>
