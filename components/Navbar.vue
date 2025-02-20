a2ecef85377e482feff29d1fe0e78958eae306ff
downtomarsguy <theonesilverest@gmail.com>
2025-02-20 14:29:49 +0530

    Navbar Updated: Discord, Github, Theme buttons added

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,15 +4,42 @@
       <img src="~/assets/dish_full_fit.png" alt="Dish Icon" class="h-8" />
       <span class="text-xl font-bold">Generic Socionics Site</span>
     </div>
+
+    <div class="flex items-center space-x-[10.5px]">
+      <button 
+        class="flex items-center justify-center bg-gradient-to-br from-[#EB00FF] to-[#5BABFF] rounded-lg p-[5px] hover:opacity-80 transition-all"
+        @click="openInNewTab('https://discord.gg/VDq4YvqrVg')"
+      >
+        <Icon name="ic:baseline-discord" class="text-[23px] cursor-pointer text-white" />
+      </button>
+
+      <button 
+        class="flex items-center justify-center border-2 border-black rounded-lg p-[3px] hover:bg-gray-100 transition-all"
+        @click="openInNewTab('https://github.com/downtomarsguy/generic-socionics-site')"
+      >
+        <Icon name="uil:github" class="text-[23px] cursor-pointer" />
+      </button>
+
+      <button 
+        class="flex items-center justify-center border-2 border-black rounded-lg p-[3px] hover:bg-gray-100 transition-all"
+        @click="toggleDarkMode"
+      >
+        <Icon :name="isDarkMode ? 'uil:moon' : 'uil:sun'" class="text-[23px] cursor-pointer" />
+      </button>
+    </div>
   </nav>
 </template>
-  
+
 <script setup>
   import { ref } from 'vue';
   
   const isDarkMode = ref(false);
-  
+
   const toggleDarkMode = () => {
     isDarkMode.value = !isDarkMode.value;
+  };
+
+  const openInNewTab = (url) => {
+    window.open(url, '_blank');
   };
 </script>
