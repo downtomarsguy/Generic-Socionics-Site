3a7323d5ffdf9250f65e667471ecd1cf3f458ca1
downtomarsguy <theonesilverest@gmail.com>
2025-02-21 09:55:34 +0530

    Index Page Updated: Title Added

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="bg-[#FFF6D4] h-screen">
     <Navbar />
-    <slot />
+    <div class="mt-28 ml-20">
+      <p class="text-6xl font-bold leading-tight">
+        A 
+        <span class="bg-clip-text text-transparent bg-gradient-to-r from-[#EB00FF] to-[#5BABFF]">
+          perpetually evolving
+        </span>
+        Socionics website
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup>
-  import Navbar from '~/components/Navbar.vue';
+  import Navbar from "~/components/Navbar.vue";
 </script>
