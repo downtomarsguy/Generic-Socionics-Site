bf61eb75d8cfe7c1d2c40084c28bd418638b4094
downtomarsguy <theonesilverest@gmail.com>
2025-02-21 10:08:30 +0530

    Index Page Updated: Site Description Added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-[#FFF6D4] h-screen">
     <Navbar />
-    <div class="mt-28 ml-20">
+    <div class="mt-28 ml-16 mr-16">
       <p class="text-6xl font-bold leading-tight">
         A 
         <span class="bg-clip-text text-transparent bg-gradient-to-r from-[#EB00FF] to-[#5BABFF]">
@@ -9,6 +9,7 @@
         </span>
         Socionics website
       </p>
+      <p class="mr-60 text-xl mt-4">Welcome to Yet Another Generic Socionics Site. This is a website for all Socionics related discussions. The site encompasses information of various schools but emphasizes on Model A (SCS) & Model G (Gulenko). This website is for both people who are extremely familiar with Socionics as well as people who are simply learning about the theory.</p>
     </div>
   </div>
 </template>
