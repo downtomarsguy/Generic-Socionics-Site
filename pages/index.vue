3c2a8bf40e5ce35deee8457b57e1faff795503fe
downtomarsguy <theonesilverest@gmail.com>
2025-02-21 10:21:40 +0530

    Index Page Updated: Page Reponsiveness Added

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="bg-[#FFF6D4] h-screen">
+  <div class="bg-[#FFF6D4] min-h-screen">
     <Navbar />
-    <div class="mt-28 ml-16 mr-16">
-      <p class="text-6xl font-bold leading-tight">
+    <div class="mt-10 sm:mt-4 md:mt-16 lg:mt-16 mx-4 sm:mx-8 md:mx-16">
+      <p class="text-4xl sm:text-5xl md:text-6xl font-bold leading-tight">
         A 
-        <span class="bg-clip-text text-transparent bg-gradient-to-r from-[#EB00FF] to-[#5BABFF]">
+        <span class="bg-clip-text leading-tight text-transparent bg-gradient-to-r from-[#EB00FF] to-[#5BABFF]">
           perpetually evolving
         </span>
         Socionics website
       </p>
-      <p class="mr-60 text-xl mt-4">Welcome to Yet Another Generic Socionics Site. This is a website for all Socionics related discussions. The site encompasses information of various schools but emphasizes on Model A (SCS) & Model G (Gulenko). This website is for both people who are extremely familiar with Socionics as well as people who are simply learning about the theory.</p>
+      <p class="text-base sm:text-lg md:text-xl mt-4 max-w-4xl pb-10">
+        Welcome to Yet Another Generic Socionics Site. This is a website for all Socionics related discussions. The site encompasses information from various schools but emphasizes Model A (SCS) & Model G (Gulenko). This website is for both people who are extremely familiar with Socionics as well as people who are simply learning about the theory.
+      </p>
     </div>
   </div>
 </template>
